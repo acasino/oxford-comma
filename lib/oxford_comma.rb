7237fb2baf5077b8cc800b1c2fe20b29aea967c2
Andrew <andrew.casin@gmail.com>
2020-10-26 23:49:19 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    #when 1-element array, return string
+    if array.length == 1
+        string = array.join
+    #when 2-element array, add "and" as separator
+    elsif array.length == 2
+        string = array.join(" and ")
+    #when 3-element array, add commas plus a final "and" before last
+    elsif array.length >= 3
+        string = array[0..-2].join(", ") + ", and " + array[-1]
+    end
+    #return array.join
+    return string
 end
